92e1662cc315543053edd45e526631ee4b3d87af
pronoy656 <pronoypaul56@gmail.com>
2024-08-18 14:49:59 +0600

    server setup done and data load from database

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+//  middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d5v1sm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+const productsCollection = client.db("jobtask").collection("products")
+
+app.get('/products', async(req,res) =>{
+    const result  = await productsCollection.find().toArray()
+    res.send(result)
+})
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res) =>{
+    res.send('Job task running')
+})
+
+app.listen(port, () =>{
+    console.log(`job task is running on port ${port}`)
+})
