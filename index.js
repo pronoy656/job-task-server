bdd6bc8bad8f0bfdda66629642ca86833bb3d771
pronoy656 <pronoypaul56@gmail.com>
2024-08-21 23:31:22 +0600

    regx implement for search

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.get('/products', async(req,res) =>{
 
 
 
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
